1a708b79b060443425d5ab25baba101bc4986d0b
gufimov <72704694+gufimov@users.noreply.github.com>
2021-01-09 14:49:34 +0530

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -25,7 +25,7 @@ class Config(object):
     EVENT_LOGS = -1001302874687  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'postgresql://Robot:asdfGHJKL@postgresql/postgres'  # needed for any database modules
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = False
